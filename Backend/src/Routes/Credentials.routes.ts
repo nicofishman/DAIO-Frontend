20e9774d05158139fc6f73d4139fecec59c75ea9
Nicolás Fishman <nicourifishman@gmail.com>
2022-05-15 16:10:22 -0300

    Moved Backend to https://github.com/nicofishman/PF-Backend

--- Backend/src/Routes/Credentials.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getSpotifyCredentials, getFirebaseCredentials } from '../Controllers/Credentials.controller';
-
-const routerCredentials = Router();
-
-routerCredentials.get('/spotify', getSpotifyCredentials);
-routerCredentials.get('/firebase', getFirebaseCredentials);
-
-export default routerCredentials;
--- Backend/src/Routes/Database.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getUsers, addUser } from '../Controllers/Database.controller';
-
-const databaseRouter = Router();
-
-databaseRouter.get('/getusers', getUsers);
-databaseRouter.post('/adduser', addUser);
-
-export default databaseRouter;
