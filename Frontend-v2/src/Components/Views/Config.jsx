7ed24e770e34f46c817c2b64f5aa3252f4b10731
Nicolás Fishman <nicourifishman@gmail.com>
2022-05-20 09:50:34 -0300

    searchThingies

--- Frontend-v2/src/Components/Views/Config.jsx
+++ Frontend-v2/src/Components/Views/Config.jsx
@@ -1,16 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View, ScrollView, Dimensions } from 'react-native'
+import React, { useState } from 'react'
 import NavBar from '../NavBar'
+import { searchTrack } from '../../Handlers/AuthHandler';
 
 
 const Config = ({ navigation, route }) => {
+    const [text, setText] = useState("");
+    const [search, setSearch] = useState({})
+    const [screenHeight, setScreenHeight] = useState(0)
+    const { height } = Dimensions.get("window");
+
+    const onContentSizeChange = (contentWidth, contentHeight) => {
+        setScreenHeight(contentHeight);
+    };
+
+
+    const onChangeText = async (e) => {
+        setText(e);
+        const res = await searchTrack(e);
+        setSearch(res);
+    }
+
+    const scrollEnabled = screenHeight > height;
+
     return (
-        <>
-            <View style={styles.container}>
-                <Text>Config</Text>
-            </View>
+        <View style={styles.container}>
+            <ScrollView
+                style={{ flex: 1, }}
+                contentContainerStyle={styles.container}
+                scrollEnabled={scrollEnabled}
+                onContentSizeChange={onContentSizeChange}
+            >
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeText}
+                    value={text}
+                />
+                <Text>{JSON.stringify(search, null, "\t")}</Text>
+            </ScrollView>
             <NavBar navigation={navigation} route={route} />
-        </>
+        </View>
     )
 }
 
@@ -23,4 +52,14 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        borderColor: '#7a42f4',
+        padding: 10,
+        borderRadius: 10,
+        backgroundColor: '#f3f3f3',
+        width: 300,
+    }
 })
