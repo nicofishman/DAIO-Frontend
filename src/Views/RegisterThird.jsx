29987f60c8f853319b5d52fa5a928876e39acc1f
nicofishman <nicourifishman@gmail.com>
2022-08-12 11:40:28 -0300

    instagram

--- src/Views/RegisterThird.jsx
+++ src/Views/RegisterThird.jsx
@@ -7,16 +7,17 @@ import * as Progress from 'react-native-progress';
 import { useRegisterContext } from '../Context/RegisterContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { addUser } from '../Handlers/AuthHandler';
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 
 const RegisterThird = ({ navigation }) => {
     const { instagram, setInstagram, artistPreference, songPreference, username, descripcion, spotifyId, avatarId, progressBar, setProgressBar } = useRegisterContext();
-    
+
     const [loaded] = useFonts({
         QuicksandRegular: require('../../assets/fonts/Quicksand/Quicksand-Regular.ttf'),
         QuicksandBold: require('../../assets/fonts/Quicksand/Quicksand-Bold.ttf'),
     });
-    
+
     const volver = () => {
         setProgressBar(0.50);
         navigation.goBack()
@@ -41,39 +42,45 @@ const RegisterThird = ({ navigation }) => {
 
     return (
         <SafeAreaView style={styles.container}>
-        {
-            loaded ? (
-                <>
-                <Progress.Bar
-                    progress={progressBar}
-                    width={windowWidth}
-                    color='rgb(94, 157, 181)'
-                    style={styles.progressBar}
-                />
-                <View style={styles.buttonBack} >
-                    <ButtonBack onPress={volver} />
-                </View>
-                <Text style={styles.textTitle}>Ingresa tu Instaram para hablar con tus match</Text>
-                <TextInput
+            {
+                loaded ? (
+                    <>
+                        <Progress.Bar
+                            progress={progressBar}
+                            width={windowWidth}
+                            color='rgb(94, 157, 181)'
+                            style={styles.progressBar}
+                        />
+                        <View style={styles.buttonBack} >
+                            <ButtonBack onPress={volver} />
+                        </View>
+                        <View style={{ flex: 1 }}>
+                            <Text style={styles.textTitle}>Ingresa tu Instaram para hablar con tus match</Text>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', borderBottomWidth: 2, borderBottomColor: '#000', width: windowWidth * .7, marginTop: 15 }}>
+                                <AntDesign style={{ marginRight: 10 }} name="instagram" size={30} color="#de4ba1" />
+                                <TextInput
                                     onChangeText={(e) => setInstagram(e)}
                                     value={instagram}
                                     caretHidden={true}
-                                    placeholder={'Ingresa tu nombre'}
+                                    placeholder={'Ingresa tu Instagram'}
                                     placeholderTextColor="#d4d4d4"
                                     spellCheck={false}
                                     autoCorrect={false}
+                                    style={styles.input}
+                                    maxLength={30}
                                 />
-                <ButtonContinue onPress={() => finishRegister()} />
+                            </View>
+                        </View>
+                        <ButtonContinue onPress={() => finishRegister()} />
 
+                    </>
 
-                </>
-                
-            ) : (
-                <ActivityIndicator />
-            )
-        }
+                ) : (
+                    <ActivityIndicator />
+                )
+            }
         </SafeAreaView>
-  )
+    )
 }
 
 export default RegisterThird
@@ -100,11 +107,21 @@ const styles = StyleSheet.create({
         position: "relative"
     },
     textTitle: {
-        top: windowHeight * 0.175,
+        marginTop: windowHeight * 0.175,
         width: windowWidth * .75,
         fontSize: 28,
         color: "#1f1f1f",
         fontFamily: 'QuicksandBold',
+    },
+    input: {
+        color: '#000',
+        paddingTop: 10,
+        paddingBottom: 10,
+        fontSize: 22,
+        width: 264,
+        height: 51,
+        position: 'relative',
+
     },
 
 })
